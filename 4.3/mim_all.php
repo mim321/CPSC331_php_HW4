a6a038a27490337ef872da785a54bab30fd3bb3a
mim321 <45837613+mim321@users.noreply.github.com>
2020-09-10 03:41:34 +0700

    Add files via upload

---
+++ 4.3/mim_all.php
@@ -0,0 +1,95 @@
+<header>
+<title>โฮมเพจของข้าพเจ้า:การสร้างตาราง</title>
+<marquee direction ="right">CPSC331_PHP Pyramid</marquee>
+  <p>....</p>
+</header>
+
+<style>
+header {
+  text-align: center;
+  padding: 0px;
+  background-color: #FFDAB9;
+  color: #000000;
+}
+</style>
+
+<style>
+    @font-face
+        {
+            font-family:TLWGTypewriter;
+            src:url('rsp_tlwgtypewriter.ttf');
+
+        }
+footer 
+	{
+   position:absolute;
+   position:absolute;
+   bottom:0;
+   left:0;
+   width:100%;
+   height:100%
+   height:50px;
+   text-align: center;
+   color: #ffffff;
+   font-weight:bold;
+   background-color: #FFDAB9;
+}
+</head>
+<body>
+    </style>
+    <pre style="font-family:TLWGTypewriter;font-size:40px;">
+        <?php echo "<br/>";
+            echo " ";
+        ?>
+    </pre>
+<tr>
+<td><center><img border="0" src="3.gif" 
+width="241" height="163"></center></td>
+</tr>
+   <CENTER> 
+   <a href="mim_1.php">Pyramid1</a>
+   <a href="mim_2.php">Pyramid2</a>
+   <a href="mim_3.php">Pyramid3</a>
+   <a href="mim_4.php">Pyramid4</a>
+   <a href="mim_5.php">Pyramid5</a>
+   <br><a href="mim_6.php">Pyramid6</a>
+   <a href="mim_7.php">Pyramid7</a>
+   <a href="mim_8.php">Pyramid8</a>
+   <a href="mim_9.php">Pyramid9</a>
+   <a href="mim_10.php">Pyramid10</a>
+   <br><a href="mim_11.php">Pyramid11</a>
+   <a href="mim_12.php">Pyramid12</a> 
+   <a href="mim_13.php">Pyramid13</a> 
+   <a href="mim_14.php">Pyramid14</a>
+   <a href="mim_15.php">Pyramid15</a> 
+   <br><a href="mim_16.php">Pyramid16</a> 
+   <a href="mim_17.php">Pyramid17</a>
+   <a href="mim_18.php">Pyramid18</a>
+   <a href="mim_19.php">Pyramid19</a>
+   <a href="mim_20.php">Pyramid20</a>
+   <br><a href="mim_21.php">Pyramid21</a>
+   <a href="mim_22.php">Pyramid22</a>
+   <a href="mim_23.php">Pyramid23</a>
+   <a href="mim_24.php">Pyramid24</a>
+   <a href="mim_25.php">Pyramid25</a>
+   <br><a href="mim_26.php">Pyramid26</a>
+   <a href="mim_27.php">Pyramid27</a>
+   <a href="mim_28.php">Pyramid28</a>
+   <a href="mim_29.php">Pyramid29</a>
+   <a href="mim_30.php">Pyramid30</a>
+   <br><a href="mim_31.php">Pyramid31</a>
+   <a href="mim_32.php">Pyramid32</a>
+   <a href="mim_33.php">Pyramid33</a>
+   <a href="mim_34.php">Pyramid34</a>
+   <a href="mim_35.php">Pyramid35</a>
+   <a href="mim_36.php">Pyramid36</a>
+   </CENTER>
+    </pre>
+<footer>
+  <p>CONTACT<br>
+  <a href="https://www.facebook.com/mim.suchicha"><img src="8.gif" width="60" height="50" alt=""></a>
+  <a href="https://github.com/mim321"><img src="7.gif" width="60" height="50" alt=""></a>
+  <a href="https://twitter.com/mymimmmm1"><img src="5.gif" width="60" height="50" alt=""></a>
+  <a href="https://www.youtube.com/channel/UCHqdeiHTJPLjayV1gW-2Y7A?view_as=subscriber"><img src="9.gif" width="60" height="50" alt=""></a></p>
+</footer>
+</body>
